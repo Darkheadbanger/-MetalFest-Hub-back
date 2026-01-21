1b60b73d69f57a10a58af1f5625682a636375872
David Bouhaben <davidbouhaben@yahoo.co.id>
2026-01-21 22:04:18 +0700

    feat: update user model to make name field optional

--- models/User.model.js
+++ models/User.model.js
@@ -13,10 +13,10 @@ const userSchema = new Schema(
       type: String,
       required: [true, "Password is required."],
     },
-    // name: {
-    //   type: String,
-    //   required: [true, "Name is required."],
-    // },
+    name: {
+      type: String,
+      required: [false, "Name is required."],
+    },
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
