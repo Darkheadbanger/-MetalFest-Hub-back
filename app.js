6802eb80f9d12d257252e13ba166fc91aea40455
David Bouhaben <davidbouhaben@yahoo.co.id>
2026-01-21 19:05:50 +0700

    feat: add debug log files and update app.js for route handling

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
+// On travaille ici:
+
 // 👇 Start handling routes here
 const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
