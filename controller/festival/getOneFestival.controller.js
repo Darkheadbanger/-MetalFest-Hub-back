f05b44f757429c0239ff68716053980f7e2ddf55
David Bouhaben <davidbouhaben@yahoo.co.id>
2026-01-23 00:10:13 +0700

    feat: implement getOneFestival function with validation and error handling

--- controller/festival/getOneFestival.controller.js
+++ controller/festival/getOneFestival.controller.js
@@ -1,5 +1,22 @@
 const Festival = require("../../models/Festival.model");
+const mongoose = require("mongoose");
 
-getOneFestival = (req, res, next) => {};
+const getOneFestival = async (req, res, next) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ messageError: "Invalid festival id format" });
+  }
+
+  try {
+    const festival = await Festival.findById(id).lean().select("-_id -__v");
+    if (!festival) {
+      return res.status(404).json({ messageError: "Festival not found" });
+    }
+    const { _id, __v, ...cleanRestFestival } = festival;
+    return res.status(200).json({ festival: cleanRestFestival });
+  } catch (error) {
+    return next(error);
+  }
+};
 
 module.exports = { getOneFestival };
