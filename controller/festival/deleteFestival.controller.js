726b0412da0fe0b8c8a891976ed79a65d205e63f
David Bouhaben <davidbouhaben@yahoo.co.id>
2026-01-23 21:59:50 +0700

    feat: implement deleteFestival function with validation for festival ID format

--- controller/festival/deleteFestival.controller.js
+++ controller/festival/deleteFestival.controller.js
@@ -1,4 +1,20 @@
 const Festival = require("../../models/Festival.model");
-deleteFestival = (req, res, next) => {};
+const mongoose = require("mongoose");
+
+const deleteFestival = async (req, res, next) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ messageError: "Invalid festival id format" });
+  }
+
+  try {
+    const deleted = await Festival.findByIdAndDelete(id);
+    return res
+      .status(200)
+      .json({ messageSuccess: "Festival deleted", festival: deleted });
+  } catch (error) {
+    return next(error);
+  }
+};
 
 module.exports = { deleteFestival };
